bf0b32001ae74448f972406fdf9bea445e2a7500
MrPogos <pogo012356@gmail.com>
2020-04-16 23:11:04 +0200

    test
    Signed-off-by: MrPogos <pogo012356@gmail.com>

--- Java/cw1/src/com/company/Main.java
+++ Java/cw1/src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
 
 public class Main {
-
+    private static String TEKST="ALa MA KOTA";
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(TEKST);
+        String s="basia ma kota";
+        printxrazy(s,3);
+
+    }
+    public static void printxrazy(String s,int liczba){
+        for (int i=0;i<liczba;i++)
+        {
+            System.out.println(s);
+        }
     }
 }
